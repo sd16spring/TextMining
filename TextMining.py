57129b16faa9e49ab276af63682a01e337ff0e51
mesrumpled <marchsaper@gmail.com>
2016-02-25 01:16:13 -0500

    Polished

--- TextMining.py
+++ TextMining.py
@@ -9,6 +9,7 @@ from pattern.web    import*
 from pattern.search import search
 from pattern.en     import parsetree 
 import pickle
+import pylab 
 
 
 def surrounding_words(L, Names, Wrongs):
@@ -21,6 +22,7 @@ def surrounding_words(L, Names, Wrongs):
     >>> surrounding_words(['sharing', 'one', 'wonderful', 'one', 'short', 'day', 'the', 'wizard', 'will', 'see', 'kristin', 'idina'], ['edna', 'idina'], ['kristin', 'chrystal'])
     []
     """
+
 
     
     name_mentions = []
@@ -35,6 +37,7 @@ def surrounding_words(L, Names, Wrongs):
 
     return one_only
     
+
 
 def adjectives(L):
     """Returns lists of adjecives present in input lists.
@@ -53,6 +56,7 @@ def adjectives(L):
             adjs.append(str(i.string))
 
     return adjs
+
 
 
 def frequency(L):
@@ -79,16 +83,42 @@ def frequency(L):
     return freq_adj
 
 
+
 def archetype(L, N, W):
     """Takes the text of a book, a character's name, and the names of other characters. 
     Returns tuples of the most commonly used adjectives to describe the character.
-    As the this only calls three doctested functions I decided to skip a doctest for this function."""
+    As the this only calls three doctested functions I decided to skip a doctest for this function.
+    """
 
     Mentions = surrounding_words(L, N, W)
     Adjectives = adjectives(Mentions)
     Freq_Adjectives = frequency(Adjectives)
 
     return Freq_Adjectives
+
+
+
+def bar_graph(T, L):
+    """Takes a list of tuples containing frequency and corresponding word.
+    Returns a bar graph of top ten most common words and their frequency.
+    There is no doctest for this function because how would you even test this.
+    """
+
+    Frequency = []
+    Adjective = []
+    for f, a in T:
+        Frequency.append(f)
+        Adjective.append(a)
+    
+    x = range(9)
+    y = Frequency[:9]
+    f = pylab.figure()
+    ax = f.add_axes([0.1, 0.1, 0.8, 0.8])
+    ax.bar(x, y, align='center')
+    ax.set_xticks(x)
+    ax.set_xticklabels(Adjective[:10])
+    pylab.title(L)
+    pylab.show()
 
 
 
@@ -107,4 +137,7 @@ if __name__ == '__main__':
     Beth = archetype(LW, All_Names[4:6], All_Names[:4] + All_Names[6:])
     Amy = archetype(LW, All_Names[6:], All_Names[:6])
 
-  
+    bar_graph(Meg, "Meg's Adjectives")
+    bar_graph(Jo, "Jo's Adjectives")
+    bar_graph(Beth, "Beth's Adjectives")
+    bar_graph(Amy, "Amy's Adjectives")
