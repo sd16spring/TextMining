18d4d94be65af426a7fc7c8ada06cb4342941c59
Gaby Clarke <gabyclarke@me.com>
2016-02-25 01:33:52 -0500

    completed plot

--- lyricAnalysis.py
+++ lyricAnalysis.py
@@ -8,17 +8,24 @@
 """
 
 import string
-import math
 import os
 from pattern.en import sentiment
-# import numpy as np
-# import matplotlib.pyplot as plt
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from pylab import *
+
 
 
 def makeLyrics(artist, fileName):
-	""" 
+	""" reads a song file and generates a list containing lines of lyrics as strings
+
+		artist: artist name
+		fileName: song file name
 		returns: a list of strings of lines of lyrics
 	"""
+	
 	f = open('./{}/Songs/{}'.format(artist, fileName), 'r')
 	content = f.read()
 	content = content.split('\n')
@@ -45,40 +52,8 @@ def cleanUpLyrics(s):
 	return wordList
 
 
-# def histogram(wordList, d):
-# 	""" takes a list of words and returns a dictionary with each word as a key to the number
-# 		of times the word appears in the list
-
-# 		wordList: a list of words
-# 		d: dictionary to store histogram in
-# 		returns: dictionary (word and frequency)
-# 	"""
-
-# 	for word in wordList:
-# 		d[word] = d.get(word, 0) + 1
-# 	return d
-
-
-# def cosineSimilarity(d1, d2):
-# 	""" adapted from http://stackoverflow.com/questions/15173225/how-to-calculate-cosine-similarity-given-2-sentence-strings-python
-
-# 		d1, d2: dictionaries of lyrics characterized by word frequencies
-# 	"""
-
-# 	intersection = set(d1.keys()) & set(d2.keys())
-# 	numerator = sum([d1[i] * d2[i] for i in intersection])
-
-# 	sum1 = sum([d1[i]**2 for i in d1.keys()])
-# 	sum2 = sum([d2[i]**2 for i in d2.keys()])
-# 	denominator = math.sqrt(sum1) * math.sqrt(sum2)
-
-# 	if not denominator: # if denominator == 0, we divide by 0
-# 		return 0.0
-# 	else:
-# 		return float(numerator) / denominator
-
-
-def remapInterval(val, inputIntervalStart, inputIntervalEnd, outputIntervalStart, outputIntervalEnd):
+def remapInterval(val, inputIntervalStart, inputIntervalEnd, outputIntervalStart, 
+					outputIntervalEnd):
     """ Given an input value in the interval [inputIntervalStart,
         inputIntervalEnd], return an output value scaled to fall within
         the output interval [outputIntervalStart, outputIntervalEnd].
@@ -102,23 +77,12 @@ def remapInterval(val, inputIntervalStart, inputIntervalEnd, outputIntervalStart
         >>> remapInterval(5, 4, 6, 1, 2)
         1.5
     """
+
     inputDelta = inputIntervalEnd - inputIntervalStart
     inputPosition = float(val - inputIntervalStart) / inputDelta
     outputDelta = outputIntervalEnd - outputIntervalStart
     outputPosition = outputIntervalStart + (inputPosition * outputDelta)
     return outputPosition
-
-
-def analyzeLyrics(artist, fileName):
-	lyrics = makeLyrics(artist, fileName)
-	dictionary = dict()
-	index = 0
-	for i in lyrics:
-		# words = cleanUpLyrics(i)
-		# histogram(words, dictionary) # assign to a variable, or put into list?
-		dictionary[index] = sentiment(i)[0]
-		index += 1
-	return averageSentiment(dictionary)
 
 
 def averageSentiment(d):
@@ -127,22 +91,39 @@ def averageSentiment(d):
 		d: a dictionary
 		returns: average of dictionary values
 	"""
+	
 	avg = sum(d.values()) / len(d)
 	return avg
 
 
-# def averageArtistSentiment(d):
-# 	""" takes a dictionary of form song : averageSongSentiment and finds the average
-# 		sentiment for all songs by the artist
+def analyzeLyrics(artist, fileName):
+	""" given a song file, analyzes song lyrics using Pattern's sentiment tool for
+		language polarity, and returns the average polarity of all lyrics in the 
+		song
 
-# 		d: dictionary of song sentiments
-# 		returns: average sentiment
-# 	"""
+		artist: artist name
+		fileName: song file name
+		returns: average polarity of all lyrics in that song (value in range -1:1)
+	"""
 
-# 	pass
+	lyrics = makeLyrics(artist, fileName)
+	dictionary = dict()
+	index = 0
+	for i in lyrics:
+		dictionary[index] = sentiment(i)[0]
+		index += 1
+	return averageSentiment(dictionary)
 
 
 def analyzeAllLyrics(artist):
+	""" takes an artist name and analyzes all songs by that artist for polarity of
+		sentiments expressed in that song lyrically
+
+		artist: artist name
+		returns: dictionary of polarity sentiment analysis for all songs, as well as
+		the average polarity of all songs
+	"""
+
 	songs = dict()
 	files = [f for f in os.listdir('./{}/Songs'.format(artist)) if '.txt' in f]
 	for file in files:
@@ -158,6 +139,16 @@ def analyzeAllLyrics(artist):
 
 
 def compare(artist, d):
+	""" takes the dictionary of polarity analysis for all songs and the average polarity
+		of all those songs and finds the absolute value of the difference between the 
+		polarity of each song and the average (ie, how different each song is from the average)
+
+		artist: artist name
+		d: dictionary of polarity sentiment analysis for all songs, as well as
+		the average polarity of all songs (from analyzeAllLyrics)
+		returns: dictionary of differences
+	"""
+
 	avg = d.get(artist)
 	print avg
 	songs = d.get('songs')
@@ -170,26 +161,40 @@ def compare(artist, d):
 
 
 def plot(artist):
+	""" uses matplotlib's scatter to plot each song by the given artist on a polar 
+		plot, where the r-component of the position is the difference between the 
+		polarity of that song and the average polarity of that artist's songs
+
+		artist: artist name
+		generates: polar plot of all songs by that artist according to the polarity
+		of the lyrics
+	"""
+
 	data = analyzeAllLyrics(artist)
-	print compare(artist, data)
+	comparison = compare(artist, data)
+
+	ax = plt.subplot(111, projection='polar')
+
+	ax.grid(True)
+	ax.set_xticklabels(['', '', '', '', '', '', '', ''])
+	ax.set_yticklabels(['', '', '', '', '', '', '', ''])
+	
+
+	deltaTheta = 2 * np.pi / len(comparison)
+	index = 0
 
 
+	for song in comparison:
+		r = comparison.get(song)
+		theta = index * deltaTheta
+		c = scatter(theta, r, s=100, linewidth=0, label='Broken Bells')
+		
+		index += 1
 
+	show()
 
 
 
 if __name__ == "__main__":
-	# analyzeAllLyrics('Broken-Bells')
-	plot('Broken-Bells')
+	# plot('Broken-Bells')
 
-
-# r = np.arange(0, 3.0, 0.01)
-# theta = 2 * np.pi * r
-
-# ax = plt.subplot(111, projection='polar')
-# ax.plot(theta, r, color='r', linewidth=3)
-# ax.set_rmax(2.0)
-# ax.grid(True)
-
-# ax.set_title("A line plot on a polar axis", va='bottom')
-# plt.show()
