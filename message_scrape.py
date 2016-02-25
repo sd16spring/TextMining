70f1975d2c8a64287763a2231ee1d4c99f42ec54
Alexander Hoppe <alexander.hoppe@students.olin.edu>
2016-02-25 12:47:57 -0500

    functionally decomposed nicely, command-line interface implemented, search still broken

--- message_scrape.py
+++ message_scrape.py
@@ -6,14 +6,25 @@ returns a word cloud of the most popular words in the thread.
 from bs4 import BeautifulSoup
 from wordcloud import WordCloud
 import string
+import sys
 
-# read in messages.htm
-bs = BeautifulSoup(open('messages.htm'), 'lxml')
-# get to the correct thread
-def get_msgs(beautifulsoup):
-    content = bs.body.contents[1].div.contents[70]
+def get_msgs(filename, threadname):
+    """
+    This function takes a filename object that is the entire message dump
+    and returns the block of messages with the thread being searched for
+    """
+    # read in file
+    bs = BeautifulSoup(open(filename), 'lxml')
+    # get to the correct thread
+    block = bs.body.contents[1].div.contents[70]
+    return block
+
+def strip_msgs(msg_block):
+    """
+    Strips all the messages out of the block of text and removes their p tags
+    """
     # get all the messages without any of the metadata
-    tagged_msgs = content.contents[2::2]
+    tagged_msgs = msg_block.contents[2::2]
     # strip p tags
     msgs = [str(m)[3:-4] for m in tagged_msgs]
     # make them into a string
@@ -21,19 +32,53 @@ def get_msgs(beautifulsoup):
 
 def word_cloud(text):
     """
-    This function makes a word_cloud object and 
+    This function makes a wordcloud object and attempts to generate a word cloud
+    using the collected messages.
     """
     wc = WordCloud()
     wc.generate(text)
 
-#assemble a frequency list
-words = dict()
-for raw in str_msgs.split():
-     word = raw.rstrip(string.punctuation).lstrip(string.punctuation).lower()
-     words[word] = words.get(word, 0) + 1
+def get_freq(text):
+    """
+    This function makes a frequency dictionary of all of the words it's given
+    """
+    words = dict()
+    for raw in text.split():
+         word = raw.rstrip(string.punctuation).lstrip(string.punctuation).lower()
+         words[word] = words.get(word, 0) + 1
+    return words
 
-output = [(words[word], word) for word in words.keys()]
-output.sort(reverse=True)
+def decorate_sort(dictionary):
+    """
+    This function takes a dictionary and sorts it, returning it as an ordered
+    list of tuples of frequency-word pairs
+    """
+    output = [(dictionary[word], word) for word in dictionary.keys()]
+    output.sort(reverse=True)
+    return output
 
-# print(str(len(output)) + " different fucking words.")
-# print("\n".join([str(e) for e in output]))
+if __name__ == '__main__':
+    #prompt user for input
+    print 'How many words to display: '
+    words = int(raw_input())
+    print 'What thread to look for: '
+    thread = raw_input()
+
+    #Extract message data
+    block = get_msgs('messages.htm', thread)
+    text = strip_msgs(block)
+
+    #try to make a word cloud
+    try:
+        word_cloud(text)
+    except ImportError as e:
+        print e
+        print "Probably a libfreetype error again"
+
+    # Generate sorted frequency list
+    freq = get_freq(text)
+    freq = decorate_sort(freq)
+
+    print 'The {} most used words in our chat are: '.format(words)
+    string_freq = [str(e) for e in freq]
+    print "\n".join(string_freq[:words])
