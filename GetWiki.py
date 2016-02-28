95159c3dbadf02c8724878b931c09fcf4b7a0cd0
lzuehsow <lydiazuehsow@gmail.comgit config --global user.name lzuehsow>
2016-02-27 23:40:19 -0500

    Latest Vers. TextMining Project

--- GetWiki.py
+++ GetWiki.py
@@ -6,11 +6,23 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 
+"""
+	This program takes a language and a Wikipedia article title as an input. It then outputs the top ten most common words within that article, first
+	checking to see if they're significant (eg: not articles or prepositions, etc.)
+
+	This program supports English, Swedish, Portugese, Hungarian, Finnish, Turkish, German, Dutch, Norwegian, Spanish, Russian, Danish, & Italian.
+
+	The language dictionary is a global because it's referenced in both get_text() and open_text(). It's a lot easier to reference and edit as a global.
+"""
+
 dict_lang = {'English':['en', 'references'], 'Swedish':['sv', 'referenser'], 'Portuguese':['pt', 'referências'], 'Hungarian':['hu', 'források'], 'Finnish':['fi', 'lähteet'], \
 	'Turkish':['tr', 'kaynakça'], 'German':['de', 'anmerkungen'], 'Dutch':['nl', 'referenties'], 'Norwegian':['nb', 'referanser'], \
-	'Catalan':['ca', 'referències'], 'Spanish':['es', 'referencias'], 'Russian':['ru', 'Примечания'.lower()], 'Danish':['da', 'referencer'], 'Italian':['it', 'bibliografia']}
+	'Spanish':['es', 'referencias'], 'Russian':['ru', 'Примечания'.lower()], 'Danish':['da', 'referencer'], 'Italian':['it', 'bibliografia']}
 
 def get_text(language, title):
+	"""
+		Finds the Wikipedia article in the specified language, then writes it into a plaintext .txt file. (Language_us_file.txt)
+	"""
 
 	filename = language + '_us_file.txt'
 
@@ -25,6 +37,10 @@ def get_text(language, title):
 	article_file.close
 
 def open_text(language):
+	"""
+		Opens the appropriate plaintext file and runs a histogram, creating a dictionary with every non-trivial word that appears and its frequency in the article.
+		Outputs a tuple of the original list and the complete sorted version.
+	"""
 	filename = language + '_us_file.txt'
 	if language in dict_lang:
 		common_words = set(stopwords.words(language.lower()))
@@ -47,7 +63,7 @@ def open_text(language):
 						else:
 							hist[word] = 1
 	return
-language = 'Spanish'
+language = 'Hungarian'
 
 get_text(language, 'United_States')
 sorted_filetext, hist = open_text(language)
