4c6e6180c132cb75cd7a912fb63b705d44e0d02e
SeanFoley123 <sean.foley@students.olin.edu>
2016-04-17 22:04:49 -0400

    Project 5 Update

--- markhov.py
+++ markhov.py
@@ -1,39 +1,43 @@
-"""Ignore quotation marks
-"""
 import random
 import pickle
-input_file = open('dickens_texts.pickle','r')
-books = pickle.load(input_file)
-punctuation = ['.', ',', '?', '!', ':', ';', '']
-order = 2
-suffix_dict = {}
-for book in books:
-	text = book.strip()
-	words = text.split(' ')												#divide it by spaces
+import string
 
-	for k in range(len(words)):
-		words[k] = words[k].replace('"', '')
-		words[k] = words[k].replace("'", "")
-
-	i = 0																
-	while i < len(words):												#separate out the punctuation
-		if len(words[i])>0:
-			last = words[i][len(words[i])-1]
-			if last in punctuation:
+def markhov(book, suffix_dict):
+	# Take a book as a string and a dictionary of prefixes mapped to suffixes
+	# and update the dictionary (memoization) with the prefixes and suffixes in the book
+	order = 2
+	suffix_dict['__order'] = order 										   # Packs up the order into the result
+	words = book.strip().split(' ')    									   # Strip out punctuation and leading/trailing whitespace, then split it based on spaces
+	i = 0
+	while i < len(words)-1:												   # My comments said to switch this to a more efficient strip, but it's what makes my 
+		if len(words[i])>0:												   # code special; I count punctuation as words. This particular method is a pretty useful
+			last = words[i][len(words[i])-1]							   # way to account for mutating a list as I iterate through it.
+			if last in string.punctuation:
 				words[i] = words[i][:len(words[i])-1]
 				words.insert(i+1, last)
 				i+=1
 		i+=1
 
-	for i in range(order):												#iterate through the different starting positions, create a dictionary of prefixs and the suffixes
-		for k in range(i, len(words)-order, order):						#go through each set of order words starting at i
-			prefix = ' '.join(words[k:k+order])
+	for i in range(order):												   # Iterate through the different starting positions
+		for k in range(i, len(words)-order, order):						   # Go through each set of order words starting at i
+			prefix = ' '.join(words[k:k+order])							   
 			suffix = words[k+order]
-			if prefix not in suffix_dict:
-				suffix_dict[prefix] = [suffix]
-			else:
-				suffix_dict[prefix].append(suffix)
+			suffix_dict[prefix] = suffix_dict.get(prefix, []) + [suffix]
 
-f = open('dictionary.pickle', 'w')
-pickle.dump(suffix_dict, f)
-f.close()
+def open_files():
+	# Loads books, which are a list of strings.
+	input_file = open('dickens_texts.pickle','r')
+	books = pickle.load(input_file)
+	return books
+
+def save_files(suffix_dict):
+	# Pickles your prefix dict
+	f = open('dictionary.pickle', 'w')
+	pickle.dump(suffix_dict, f)
+	f.close()
+
+books = open_files()
+suffix_dict = {}
+for book in books:
+	markhov(book, suffix_dict)
+save_files(suffix_dict)
