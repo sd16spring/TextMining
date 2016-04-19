d8ea5abcee38672ba556c25807791e8771f96321
Lauren Gulland <lauren.gulland@students.olin.edu>
2016-04-19 01:11:12 -0400

    consolidated a lot of code, made it much easier to read. Restructured some of the basic framework.

--- histogram_analysis.py
+++ histogram_analysis.py
@@ -3,118 +3,75 @@ import pickle
 import string
 from bokeh.charts import Bar, output_file, show
 from pandas import *
+
 '''
+BEFORE RUNNING: 
 Be sure to have pickled copies of each of the books in the same folder. 
 Obtain these pickled copies of books by running functions as necessary in pickle_file_creation.py
 '''
+plays = ['Hamlet','Romeo','Lear', 'Caesar'] #list of names of plays. This order must be consistent in all following aggregated lists.
+pickled_filenames = ['hamlet.pickle','romeo.pickle','lear.pickle','caesar.pickle']
 
-#Make string copies of books 
-hamlet_file = open('hamlet.pickle','r')
-copy_of_hamlet = pickle.load(hamlet_file)
-
-romeo_file = open('romeo.pickle','r')
-copy_of_romeo = pickle.load(romeo_file)
-
-lear_file = open('lear.pickle','r')
-copy_of_lear = pickle.load(lear_file)
-
-caesar_file = open('caesar.pickle','r')
-copy_of_caesar = pickle.load(caesar_file)
-
+#Make string copies of books and add them into a list of copies of the books
+copy_of_texts = []
+for i in pickled_filenames:
+	with open(i, 'r') as f:
+		copy_of_texts.append(pickle.load(f))
 
 def most_frequent(s):
-    ''' returns dictionary of top 15 words in each book and their percentage-frequencies
-    if chose to return 'rtrn': returns list of words in decreasing order based on frequency of appearances in a string
-    '''
-    s = s.lower()
-    s = s.translate(string.maketrans('',''), string.punctuation)
-    totalWords = len(s)
-    d = {}
-    for word in s.split():
-        d[word] = d.get(word, 0)+1
-    wordList = []
-    for word in d:
-        wordList.append((d[word], word))
-    wordList.sort(reverse=True)
-    rtrn = []
-    i = 0
-    top15 = {}
-    for frequency, word in wordList:
-        rtrn.append(word)
-        if i < 50:
-        	i += 1
-        	top15[word] = 100.0*frequency/totalWords
-        	#print '%s %s' %(100.0*frequency/totalWords, word)
-        else:
-        	break
-    return top15
+	''' returns dictionary of top 15 words in each book and their percentage-frequencies
+	if chose to return 'total_top': returns list of words in decreasing order based on frequency of appearances in a string
+	'''
+	s = s.lower()
+	s = s.translate(string.maketrans('',''), string.punctuation)
+	totalWords = len(s)
+	d = {}
+	for word in s.split():
+		d[word] = d.get(word, 0)+1
+	wordList = []
+	for word in d:
+		wordList.append((d[word], word))
+	wordList.sort(reverse=True)
+	total_top = []
+	i = 0
+	top15 = {}
+	for frequency, word in wordList[:50]:
+		total_top.append(word)
+		top15[word] = 100.0*frequency/totalWords
+	return top15
 
+def merge_lists(partial_lists):
+	#create a set of all of the words most frequently used across all of the plays
+	all_words = []
+	for i in partial_lists: #concatenates lists into one
+		all_words+=i
+	all_words_set = set(all_words) #removes duplicates
+	all_words = list(all_words_set) #turns back into a list
+	all_words.sort() #alphabetically sorts list of all words
+	return all_words
 
-#print 'Top 15 Words Used in Oliver Twist:'
-#top15_oliver = most_frequent(copy_of_oliver_twist)
-
-#print 'Top 15 Words Used in Wizard of Oz:'
-#top15_wizard = most_frequent(copy_of_wizard_of_oz)
-
-#print 'Top 15 Words Used in Hamlet:'
-top15_hamlet = most_frequent(copy_of_hamlet)
-
-#print 'Top 15 Words Used in Romeo and Juliet:'
-top15_romeo = most_frequent(copy_of_romeo)
-
-#print 'Top 15 Words Used in King Lear:'
-top15_lear = most_frequent(copy_of_lear)
-
-#print 'Top 15 Words Used in Julius Caesar:'
-top15_caesar = most_frequent(copy_of_caesar)
-
-
-#create a set of all of the words most frequently used across all of the plays
-all_words = []
-for word in top15_hamlet:
-	all_words.append(word)
-for word in top15_romeo:
-	all_words.append(word)
-for word in top15_lear:
-	all_words.append(word)
-for word in top15_caesar:
-	all_words.append(word)
-
-all_words_set = set(all_words) #removes duplicates
-all_words = list(all_words_set)
-all_words.sort()
-plays = ['Hamlet','Romeo','Lear', 'Caesar']
-#print all_words
+top15_lists = [most_frequent(i) for i in copy_of_texts]
+all_words = merge_lists(top15_lists)
 
 #create a dataframe
 df = DataFrame(0, index=all_words, columns=plays)
 
 for i in range(-1,len(all_words)):
-	#print i
-	#print all_words[i]
-	#print top15_hamlet.get(all_words[i], 0)
-	df.loc[all_words[i]]['Hamlet'] = top15_hamlet.get(all_words[i], 0)
-	df.loc[all_words[i]]['Romeo'] = top15_romeo.get(all_words[i], 0)
-	df.loc[all_words[i]]['Lear'] = top15_lear.get(all_words[i], 0)
-	df.loc[all_words[i]]['Caesar'] = top15_caesar.get(all_words[i], 0)
+	for j in range(0,len(plays)):
+		df.loc[all_words[i],plays[j]] = top15_lists[j].get(all_words[i], 0)
+		df.set_value(i, plays[j], top15_lists[j].get(all_words[i], 0))
 
-	df.set_value(i, 'Hamlet', top15_hamlet.get(all_words[i], 0))
-	df.set_value(i, 'Romeo', top15_romeo.get(all_words[i], 0))
-	df.set_value(i, 'Lear', top15_lear.get(all_words[i], 0))
-	df.set_value(i, 'Caesar', top15_caesar.get(all_words[i], 0))
 dupl_df = df.ix[:all_words[-1]]
 print
 print 'Most common words in each play by percentage-frequencies'
-#print dupl_df
+print dupl_df
 
 #Pickle the dataframe to a text file
-f = open('pandaframe.pickle','w')
-pickle.dump(dupl_df, f)
-f.close()
+# with open('pandaframe.pickle','w') as f:
+# 	pickle.dump(dupl_df, f)
 
-panda_file = open('pandaframe.pickle','r')
-copy_of_df = pickle.load(panda_file)
-print copy_of_df
+# with open('pandaframe.pickle','r') as panda_file:
+# 	copy_of_df = pickle.load(panda_file)
 
 ''' Attempted to make a bar graph using Panda dataframes...but didn't have enough time and needed actual sleep
 '''
