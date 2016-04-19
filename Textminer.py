4ace6c17ef27312be06dad6f8b501c8bc88aea3b
cm898946 <cm898946@gmail.com>
2016-04-18 22:53:39 -0400

    Finished MP5

--- Textminer.py
+++ Textminer.py
@@ -10,20 +10,21 @@ from pickle import dump, load
 from pattern.en import *
 
 months = {  'january':1,
-			'february':2,
-			'march':3,
-			'april':4,
-			'may':5,
-			'june':6,
-			'july':7,
-			'august':8,
-			'september':9,
-			'october':10,
-			'november':11,
-			'december':12
-		}
-
+				'february':2,
+				'march':3,
+				'april':4,
+				'may':5,
+				'june':6,
+				'july':7,
+				'august':8,
+				'september':9,
+				'october':10,
+				'november':11,
+				'december':12
+			}
+main_url = '/blog/the-olinsider/'
 folder = 'Data/'
+# This statement checks if the forlder exists, if it doesn't it will make it
 
 def save_data(dat, filename):
 	''' This function takes in data and a file name
@@ -56,6 +57,8 @@ def mine_olinsider(url, mine=False):
 		If it does not exist, it will download the webpage sourcecode and save the data
 		Otherwise, it will return what it got from get_data
 		'''
+	'''if not exists(self.folder):
+    	os.makedirs(self.folder)'''
 	filename = 'Data'+url.replace('/', '')+'.dat'
 	if get_data(filename) == None or mine == True:
 		print('getting data')
@@ -76,8 +79,7 @@ def mine():
 		It will always redownload the main page and latest month in case another article has been added within that month
 	'''
 	#getting the main Olinsider page
-	url = '/blog/the-olinsider/'
-	main_page = mine_olinsider(url, True)
+	main_page = mine_olinsider(main_url, True)
 	dat = main_page.split('\"')
 	#searching through the mainpage source code to find the articles by month urls
 	month_urls = [d.split('::')[1] for d in dat if '/blog/the-olinsider/by-date' in d]
@@ -231,131 +233,135 @@ def get_sentiment(posts):
 		subjectivity.append(sent[1])
 	return polarity, subjectivity
 
-def process_dates(posts, polarity, subjectivity):
-	''' This function takes in a list of posts, polarity, and subjectivity
+class grapher():
+	'''This class was created to reduce identical code and for the same data set being passed to the differenct graphing functionss
 
-		It gets the dates from the posts and returns a list of the year, month, polarity, and subjectivity of each post
+		bar_plot is called twice with different data sets instead of writing two sepereate 
 	'''
-	dates = [post[0] for post in posts]
-	for i in range(len(dates)):
-		dates[i] = [dates[i][2], dates[i][0], polarity[i], subjectivity[i]]
-	return dates
+	def __init__(self, posts, polarity, subjectivity):
+		self.posts = posts
+		self.polarity = polarity
+		self.subjectivity = subjectivity
+		self.process_dates()
+		self.get_by_month()
+		self.setup_by_type()
 
-def get_by_month(date_data):
-	'''This function resorts the data for the bar plots'''
-	rang = date_data[-1][0] - date_data[0][0]
-	years = []
-	for i in range(rang+1):
-		months = []
-		for date in date_data:
-			if date[0] == i + date_data[0][0]:
-				months.append(date[1:])
-		years.append(sorted(months))
+	def process_dates(self):
+		''' This function takes in a list of posts, polarity, and subjectivity
 
-	bymonth = []
-	for year in years:
-		mon = []
-		for i in range(1,13):
-			counter = 0
-			polarity = 0
-			subjectivity = 0
+			It gets the dates from the posts and returns a list of the year, month, polarity, and subjectivity of each post
+		'''
+		dates = [post[0] for post in self.posts]
+		for i in range(len(dates)):
+			dates[i] = [dates[i][2], dates[i][0], self.polarity[i], self.subjectivity[i]]
+		self.date_data = sorted(dates)
+
+	def get_by_month(self):
+		'''This function resorts the data for the bar plots'''
+		rang = self.date_data[-1][0] - self.date_data[0][0]
+		years = []
+		for i in range(rang+1):
+			months = []
+			for date in self.date_data:
+				if date[0] == i + self.date_data[0][0]:
+					months.append(date[1:])
+			years.append(sorted(months))
+
+		bymonth = []
+		for year in years:
+			mon = []
+			for i in range(1,13):
+				counter = 0
+				polarity = 0
+				subjectivity = 0
+				for month in year:
+					if month[0] == i:
+						polarity += month[1]
+						subjectivity += month[2]
+						counter +=1
+				if counter > 0:
+					mon.append([float(polarity/counter), float(subjectivity/counter)])
+				else:
+					mon.append([0, 0])
+			bymonth.append(mon)
+		self.bymonth = bymonth
+
+	def setup_by_type(self):
+		'''This function sets up the type_by_month lists for the bar graphs'''
+		self.pol_bymonth = []
+		self.sub_bymonth = []
+		for year in self.bymonth:
+			mon = []
+			mon2 = []
 			for month in year:
-				if month[0] == i:
-					polarity += month[1]
-					subjectivity += month[2]
-					counter +=1
-			if counter > 0:
-				mon.append([float(polarity/counter), float(subjectivity/counter)])
-			else:
-				mon.append([0, 0])
-		bymonth.append(mon)
-	return bymonth
+				mon.append(month[0])
+				mon2.append(month[1])
+			self.pol_bymonth.append(mon)
+			self.sub_bymonth.append(mon2)
 
-def scatter_plot(polarity, subjectivity):
-	'''This function plots and saves a scatter plot of the subjectivity vs polarity'''
-	plt.scatter(polarity, subjectivity, marker = '*', color = 'g')
-	plt.axis([-1, 1, 0, 1])
-	plt.grid(True)
-	plt.xlabel('Polarity (Negative to Positive)')
-	plt.ylabel('Subjectivity (Objective to Subjective)')
-	plt.title('Polarity vs Subjectivity for Olinsider posts')
-	plt.savefig('ScatterPlot.eps')
-	plt.clf()
+	def scatter_plot(self):
+		'''This function plots and saves a scatter plot of the subjectivity vs polarity'''
+		plt.scatter(self.polarity, self.subjectivity, marker = '*', color = 'g')
+		plt.axis([-1, 1, 0, 1])
+		plt.grid(True)
+		plt.xlabel('Polarity (Negative to Positive)')
+		plt.ylabel('Subjectivity (Objective to Subjective)')
+		plt.title('Polarity vs Subjectivity for Olinsider posts')
+		plt.savefig('ScatterPlot.eps')
+		plt.clf()
 
-def scatter_plot_time(bymonth, polarity, subjectivity):
-	'''This function plots and saves a scatter plot of the subjectivity and polarity vs time'''
-	avg_pol = []
-	avg_sub = []
-	for year in bymonth:
-		for month in year:
-			avg_pol.append(month[0])
-			avg_sub.append(month[1])
-	pol = plt.scatter(range(len(avg_pol)), avg_pol, marker = '*', color = 'g')
-	sub = plt.scatter(range(len(avg_pol)), avg_sub, marker = 'o', color = 'b')
-	plt.axis([0, len(avg_pol), 0, 1])
-	plt.xticks(np.arange(0, len(avg_pol)+1, 12))
-	plt.xlabel('Months since the first Olinsider post')
-	plt.ylabel('Subjectivity/Positivity')
-	plt.title('Polarity and Subjectivity vs time for Olinsider posts')
-	plt.legend((pol, sub), ('Average Positivity', 'Average Subjectivity'), scatterpoints = 1, loc = 'upper center')
-	plt.savefig('ScatterOverTime.eps')
-	plt.clf()
+	def scatter_plot_time(self):
+		'''This function plots and saves a scatter plot of the subjectivity and polarity vs time'''
+		avg_pol = []
+		avg_sub = []
+		for year in self.bymonth:
+			for month in year:
+				avg_pol.append(month[0])
+				avg_sub.append(month[1])
+		pol = plt.scatter(range(len(avg_pol)), avg_pol, marker = '*', color = 'g')
+		sub = plt.scatter(range(len(avg_pol)), avg_sub, marker = 'o', color = 'b')
+		plt.axis([0, len(avg_pol), 0, 1])
+		plt.xticks(np.arange(0, len(avg_pol)+1, 12))
+		plt.xlabel('Months since the first Olinsider post')
+		plt.ylabel('Subjectivity/Positivity')
+		plt.title('Polarity and Subjectivity vs time for Olinsider posts')
+		plt.legend((pol, sub), ('Average Positivity', 'Average Subjectivity'), scatterpoints = 1, loc = 'upper center')
+		plt.savefig('ScatterOverTime.eps')
+		plt.clf()
 
-def bar_plot(name, type_bymonth):
-	'''This function creates a bargraph based on the given data'''
-	n = 9
-	ind = np.arange(12)
-	width = 1./(1+n)
-	colors = ['#000000', '#FE2E2E', '#FF8000', '#FFFF00', '#74DF00', '#01DFA5', '#00BFFF', '#0101DF', '#8904B1', '#DF0174']
-	labels = range(2007,2017) 
-	ticks = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-	fig = plt.figure()
-	ax = fig.add_subplot(111)
-	for year in range(len(type_bymonth)):
-		ax.bar(ind+year*width, type_bymonth[year], width, color = colors[year], label = str(labels[year]))
-	plt.xticks(ind+n/2.*width, ticks)
-	ax.tick_params(axis='x', labelsize=8)
-	plt.gcf().subplots_adjust(right=0.85)
-	plt.xlabel('Month')
-	plt.ylabel(name)
-	plt.title('Average ' + name + ' for a given month and year')
-	plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-	fig.set_size_inches(9, 5)
-	fig.savefig(name + 'Bar.eps')
-	plt.clf()
+	def bar_plot(self, name, type_bymonth):
+		'''This function creates a bargraph based on the given data'''
+		n = 9
+		startyear = 2007
+		ind = np.arange(12)
+		width = 1./(1+n)
+		colors = ['#000000', '#FE2E2E', '#FF8000', '#FFFF00', '#74DF00', '#01DFA5', '#00BFFF', '#0101DF', '#8904B1', '#DF0174']
+		labels = range(startyear,startyear+n+1) 
+		ticks = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+		for year in range(len(type_bymonth)):
+			ax.bar(ind+year*width, type_bymonth[year], width, color = colors[year], label = str(labels[year]))
+		plt.xticks(ind+n/2.*width, ticks)
+		ax.tick_params(axis='x', labelsize=8)
+		plt.gcf().subplots_adjust(right=0.85)
+		plt.xlabel('Month')
+		plt.ylabel(name)
+		plt.title('Average ' + name + ' for a given month and year')
+		plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+		fig.set_size_inches(9, 5)
+		fig.savefig(name + 'Bar.eps')
+		plt.clf()
 
-def run():
-	''' This function runs the mining part of the program
-
-		returns the data_data, polarity, and subjectivity
-	'''
+if __name__ == '__main__':
+	doctest.testmod()
 	source = mine()
 	filtered_posts = filter_unicode(source)
 	posts = update_data(filtered_posts, 'Filtered_Posts.dat')
 	real_posts = process_posts(posts)
 	polarity, subjectivity = get_sentiment(real_posts)
-	date_data = process_dates(real_posts, polarity, subjectivity)
-	date_data = sorted(date_data)
-	return date_data, polarity, subjectivity
-
-if __name__ == '__main__':
-	doctest.testmod()
-	date_data, polarity, subjectivity = run()
-	bymonth = get_by_month(date_data)
-	scatter_plot(polarity, subjectivity)
-	scatter_plot_time(bymonth, polarity, subjectivity)
-
-	#Sets up the data for the bar graphs
-	pol_bymonth = []
-	sub_bymonth = []
-	for year in bymonth:
-		mon = []
-		mon2 = []
-		for month in year:
-			mon.append(month[0])
-			mon2.append(month[1])
-		pol_bymonth.append(mon)
-		sub_bymonth.append(mon2)
-
-	bar_plot('Polarity', pol_bymonth)
-	bar_plot('Subjectivity', sub_bymonth)
+	g = grapher(real_posts, polarity, subjectivity)
+	g.scatter_plot()
+	g.scatter_plot_time()
+	g.bar_plot('Polarity', g.pol_bymonth)
+	g.bar_plot('Subjectivity', g.sub_bymonth)
